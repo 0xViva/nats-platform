5cb1b1c9247682010f7302276ba1c4f05e5f4bb3
0xViva <0xViva@proton.me>
2026-06-28 13:48:19 +0200

    make server command better.

--- server.sh
+++ server.sh
@@ -1,1 +1,11 @@
-docker run -p 4222:4222 -p 8222:8222 nats:latest -js 
+#!/bin/bash
+
+mkdir -p jetstream
+
+docker run -d --name nats-server \
+  -p 4222:4222 \
+  -p 8222:8222 \
+  -v "$PWD/nats.conf:/etc/nats/nats.conf" \
+  -v "$PWD/jetstream:/data" \
+  nats:latest \
+  -c /etc/nats/nats.conf
